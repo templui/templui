2f424ad03ca5f5cec7bec60055dccc37e32dadd5
Diana Suvorova <diana.suvorova@gmail.com>
2025-04-13 18:19:16 -0500

    chore: adding remaining showcases

--- eslint.config.js
+++ eslint.config.js
@@ -12,11 +12,6 @@ module.exports = [
     },
     "rules": {
       ...htmlPlugin.configs.recommended.rules,
-      "@html-eslint/element-newline": "off",
-      "@html-eslint/indent": "off",
-      "@html-eslint/attrs-newline": "off",
-      "@html-eslint/attrs-newline": "off",
-      "@html-eslint/no-extra-spacing-attrs": "off",
     }
   },
 ]
