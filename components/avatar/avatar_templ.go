1e2ae4b91ce080d4284d0629595813a7b1464e9a
axadrn <hello@axeladrian.com>
2026-03-09 19:10:28 +0200

    refactor: remove exported component script API and simplify JS loading

--- components/avatar/avatar_templ.go
+++ components/avatar/avatar_templ.go
@@ -51,7 +51,7 @@ func Avatar(props ...Props) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = Script().Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = script().Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -342,7 +342,7 @@ func Fallback(props ...FallbackProps) templ.Component {
 
 var onceHandle = templ.NewOnceHandle()
 
-func Script() templ.Component {
+func script() templ.Component {
 	return templruntime.GeneratedTemplate(func(templ_7745c5c3_Input templruntime.GeneratedComponentInput) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_W, ctx := templ_7745c5c3_Input.Writer, templ_7745c5c3_Input.Context
 		if templ_7745c5c3_CtxErr := ctx.Err(); templ_7745c5c3_CtxErr != nil {
